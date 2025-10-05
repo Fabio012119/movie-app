352209cf2339eb852e05b8dcd04346c89081ddde
Fabio <fabiooosara@gmail.com>
2025-10-05 20:33:16 +0300

    add base url in const

--- utils/general.ts
+++ utils/general.ts
@@ -1,4 +1,4 @@
-export const BASE = process.env.API_URL?.replace(/\/$/, "");
+export const BASE = "https://movie-api-decs.onrender.com/api";
 
 export function cn(...parts: Array<string | false | null | undefined>) {
   return parts.filter(Boolean).join(" ");
